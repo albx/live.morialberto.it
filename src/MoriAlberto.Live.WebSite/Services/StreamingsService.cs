acd13cd8db4ef002ba25b64589fd105953aaf61c
Alberto Mori <albi.mo87@hotmail.it>
2024-01-16 22:18:55 +0100

    #5 - WIP integrazione data api builder e graphql

--- src/MoriAlberto.Live.WebSite/Services/StreamingsService.cs
+++ src/MoriAlberto.Live.WebSite/Services/StreamingsService.cs
@@ -1,15 +1,14 @@
-﻿using Markdig;
-using MoriAlberto.Live.Models;
+﻿using MoriAlberto.Live.Models;
+using MoriAlberto.Live.WebSite.Client;
 using MoriAlberto.Live.WebSite.Model;
-using System.Net.Http.Json;
 
 namespace MoriAlberto.Live.WebSite.Services;
 
 public class StreamingsService
 {
-    public HttpClient Client { get; }
+    public StreamingsClient Client { get; }
 
-    public StreamingsService(HttpClient client)
+    public StreamingsService(StreamingsClient client)
     {
         Client = client ?? throw new ArgumentNullException(nameof(client));
     }
@@ -18,8 +17,21 @@ public class StreamingsService
     {
         var model = new IndexViewModel();
 
-        var streamings = await Client.GetFromJsonAsync<IEnumerable<StreamingList.StreamingListItem>>("api/streamings/scheduled");
-        model.Streamings = streamings ?? Array.Empty<StreamingList.StreamingListItem>();
+        var queryResult = await Client.GetScheduledStreamings.ExecuteAsync();
+        model.Streamings = queryResult.Data?
+            .Streamings
+            .Items
+            .Select(s => new StreamingList.StreamingListItem
+            {
+                Title = s.Title,
+                EndTime = TimeOnly.Parse(s.EndingTime),
+                ScheduleDate = DateOnly.FromDateTime(s.ScheduleDate.DateTime),
+                Slug = s.Slug,
+                StartTime = TimeOnly.Parse(s.StartingTime)
+            }) ?? [];
+
+        //var streamings = await Client.GetFromJsonAsync<IEnumerable<StreamingList.StreamingListItem>>("api/streamings/scheduled");
+        //model.Streamings = streamings ?? Array.Empty<StreamingList.StreamingListItem>();
 
         return model;
     }
@@ -36,11 +48,11 @@ public class StreamingsService
             }
         }
 
-        var streamingList = await Client.GetFromJsonAsync<StreamingList>(url);
+        //var streamingList = await Client.GetFromJsonAsync<StreamingList>(url);
         var model = new ArchiveViewModel
         {
-            Streamings = streamingList?.Streamings ?? Array.Empty<StreamingList.StreamingListItem>(),
-            NumberOfPages = streamingList?.NumberOfPages ?? 0
+            Streamings = [], //streamingList?.Streamings ?? Array.Empty<StreamingList.StreamingListItem>(),
+            NumberOfPages = 0 //streamingList?.NumberOfPages ?? 0
         };
 
         return model;
@@ -48,22 +60,23 @@ public class StreamingsService
 
     public async Task<LiveDetailViewModel?> GetStreamingDetailAsync(string slug)
     {
-        var streaming = await Client.GetFromJsonAsync<Streaming>($"api/streamings/detail/{slug}");
-        if (streaming is null)
-        {
-            return null;
-        }
+        return new();
+        //var streaming = await Client.GetFromJsonAsync<Streaming>($"api/streamings/detail/{slug}");
+        //if (streaming is null)
+        //{
+        //    return null;
+        //}
 
-        return new LiveDetailViewModel
-        {
-            Abstract = Markdown.ToHtml(streaming.Abstract),
-            EndTime = streaming.EndingTime,
-            ScheduleDate = streaming.ScheduleDate,
-            Seo = streaming.Seo,
-            StartTime = streaming.StartingTime,
-            Title = streaming.Title,
-            TwitchUrl = streaming.HostingChannelUrl,
-            YouTubeUrl = streaming.YouTubeVideoUrl?.Replace("https://youtu.be", "https://www.youtube.com/embed")
-        };
+        //return new LiveDetailViewModel
+        //{
+        //    Abstract = Markdown.ToHtml(streaming.Abstract),
+        //    EndTime = streaming.EndingTime,
+        //    ScheduleDate = streaming.ScheduleDate,
+        //    Seo = streaming.Seo,
+        //    StartTime = streaming.StartingTime,
+        //    Title = streaming.Title,
+        //    TwitchUrl = streaming.HostingChannelUrl,
+        //    YouTubeUrl = streaming.YouTubeVideoUrl?.Replace("https://youtu.be", "https://www.youtube.com/embed")
+        //};
     }
 }
